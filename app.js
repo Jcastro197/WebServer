874aac38392835c0552bc5a95d290f778f03333c
jcastro197 <jcastro2022@fau.edu>
2023-01-28 12:32:54 -0500

    Learning npm init

--- app.js
+++ app.js
@@ -2,24 +2,31 @@ const http = require('http');
 const fs = require('fs');
 const port = 5000;
 const express = require('express');
+const app = express();
 
-const server = http.createServer((req,res) => {
-    res.writeHead(200,{'Content-Type':'text/html'})
-    fs.readFile('index.html',(err,data) => {
-        if(err) {
-            res.writeHead(404);
-            res.write('Error: File Not Found');
-        } else {
-            res.write(data);
-        }
-        res.end();
-    });
+app.listen(port,() => {
+    console.log(`Server is running on port ${port}`);
 });
 
-server.listen(port,(err) => {
-    if(err) {
-        console.log(err);
-    } else {
-        console.log(`Server is running on port ${port}`);
-    }
-});
+app.use(express.static('public'));
+
+// const server = http.createServer((req,res) => {
+//     res.writeHead(200,{'Content-Type':'text/html'})
+//     fs.readFile('index.html',(err,data) => {
+//         if(err) {
+//             res.writeHead(404);
+//             res.write('Error: File Not Found');
+//         } else {
+//             res.write(data);
+//         }
+//         res.end();
+//     });
+// });
+
+// server.listen(port,(err) => {
+//     if(err) {
+//         console.log(err);
+//     } else {
+//         console.log(`Server is running on port ${port}`);
+//     }
+// });
